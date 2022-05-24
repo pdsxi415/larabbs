9ad990285cce6bef7b82dd2da05227f5dedb9bcf
vagrant <vagrant@homestead>
2022-05-24 12:26:25 +0000

    用户可以发表评论

--- routes/web.php
+++ routes/web.php
@@ -40,4 +40,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //帖子发布图片上传
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
